ef2e7a201ffab8d0446dce0c69faef375082707a
Abu Shahriar Ratul <ratulcse10@gmail.com>
2015-09-05 03:44:27 +0600

    local database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'scdn_carnival',
-			'username'  => 'scdn_ratul',
-			'password'  => '!@#123!@#',
+			'database'  => 'carnival',
+			'username'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
