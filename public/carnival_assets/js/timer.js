b306fb6e97bc2e0af5f9890fbf753ec8f56adb80
Abu Shahriar Ratul <ratulcse10@gmail.com>
2015-10-05 22:40:51 +0600

    starting date countdown fix

--- public/carnival_assets/js/timer.js
+++ public/carnival_assets/js/timer.js
@@ -11,7 +11,7 @@ time zone offset.
 var current="Winter is here!";   //-->enter what you want the script to display when the target date and time are reached, limit to 20 characters
 var year=2015;    //-->Enter the count down target date YEAR
 var month=11;      //-->Enter the count down target date MONTH
-var day=6;       //-->Enter the count down target date DAY
+var day=27;       //-->Enter the count down target date DAY
 var hour=8;      //-->Enter the count down target date HOUR (24 hour clock)
 var minute=00;    //-->Enter the count down target date MINUTE
 var tz=-5;        //-->Offset for your timezone in hours from UTC (see http://wwp.greenwichmeantime.com/index.htm to find the timezone offset for your location)
