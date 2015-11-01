ff85485eacbba03d3211d839b5e3b0a87db39eb7
Abu Shahriar Ratul <ratulcse10@gmail.com>
2015-11-01 13:44:15 +0600

    gc dl fix

--- app/views/site/gaming_contest.blade.php
+++ app/views/site/gaming_contest.blade.php
@@ -197,15 +197,11 @@
                                 <td>Registration Start</td> 
                             </tr>
                             <tr>
-                                <td>31 October 2015</td>
+                                <td><strike>31 October 2015</strike>15 November 2015</td>
                                 <td>Registration Close</td> 
                             </tr>
                             <tr>
-                                <td>1 November 2015</td>
-                                <td>Selected Participants List will be Published</td> 
-                            </tr>
-                            <tr>
-                                <td>5 November 2015</td>
+                                <td><strike>5 November 2015</strike>15 November 2015</td>
                                 <td>Payment Deadline</td> 
                             </tr>
                             <tr>
